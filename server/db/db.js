328b42f3aa624537c449410c308151ffc3f8a9f6
minot <cosimo.togna@fao.org>
2018-03-07 13:10:34 +0100

    updated db config

--- server/db/db.js
+++ server/db/db.js
@@ -2,15 +2,23 @@
 
 const pgp = require('pg-promise')({})
 
-const config = {
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-}
+const config = process.env.DATABASE_URL
+  ? {
+    connectionString: process.env.DATABASE_URL,
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    ssl: process.env.PGSSL === 'true',
+  }
+  : {
+    user: process.env.PGUSER,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    ssl: process.env.PGSSL === 'true',
+  }
 
 const db = pgp(config)
 
